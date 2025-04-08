5e1f9500a73738d93d3bbce5dbdca796f8f8097d
RKA2 <rka2@ford.com>
2025-04-08 21:52:11 +0530

    initial Commit

--- src/main/java/org/example/backendmovieticketbooking/entities/Theater.java
+++ src/main/java/org/example/backendmovieticketbooking/entities/Theater.java
@@ -1,4 +1,26 @@
 package org.example.backendmovieticketbooking.entities;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Theater {
+    @Id
+    private int theaterId;
+    private String theaterName;
+    private int totalSeats;
+    @ElementCollection
+    private List<String> Timing;
+
+
 }
