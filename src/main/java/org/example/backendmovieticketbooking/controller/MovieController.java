4b8a2d8ea4523f192e5c4e9d998caa4bda1b71a1
RKA2 <rka2@ford.com>
2025-04-09 11:36:05 +0530

    mentor changes

---
+++ src/main/java/org/example/backendmovieticketbooking/controller/MovieController.java
@@ -0,0 +1,50 @@
+package org.example.backendmovieticketbooking.controller;
+
+
+import org.example.backendmovieticketbooking.entities.Movie;
+import org.example.backendmovieticketbooking.service.IMovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movie")
+public class MovieController {
+
+    @Autowired
+    private IMovieService movieService;
+
+    @PostMapping("/")
+    public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
+        Movie createdMovie = movieService.addMovie(movie);
+        return ResponseEntity.ok(createdMovie);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Integer id) {
+        Movie movie = movieService.getMovieById(id);
+        return ResponseEntity.ok(movie);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieService.getAllMovies();
+        return ResponseEntity.ok(movies);
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Movie> updateMovie(@RequestBody Movie movie) {
+        Movie updatedMovie = movieService.updateMovie(movie);
+        return ResponseEntity.ok(updatedMovie);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteMovie(@PathVariable Integer id) {
+        if (movieService.deleteMovie(id)) {
+            return "successfully deleted";
+        }
+        return "Movie Not Found";
+    }
+}
---
+++ src/main/java/org/example/backendmovieticketbooking/controller/TheaterController.java
@@ -0,0 +1,46 @@
+package org.example.backendmovieticketbooking.controller;
+
+
+import org.example.backendmovieticketbooking.entities.Theater;
+import org.example.backendmovieticketbooking.service.ITheaterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/theater")
+public class TheaterController {
+    @Autowired
+    private ITheaterService theaterService;
+
+    @PostMapping("/")
+    public Theater addStudent(@RequestBody Theater theater) {
+        return theaterService.addTheater(theater);
+    }
+
+    @PutMapping("/")
+    public Theater updateTheater(@RequestBody Theater theater) {
+        return theaterService.updateTheater(theater);
+    }
+
+    @DeleteMapping("/{theaterId}")
+    public String deleteTheater(@PathVariable int theaterId) {
+        if(theaterService.deleteTheater(theaterId)){
+            return "Theater deleted successfully";
+        }
+        return "Theater not found";
+    }
+
+    @GetMapping("/{theaterId}")
+    public Theater getTheater(@PathVariable int theaterId) {
+        return theaterService.getTheater(theaterId);
+    }
+
+    @GetMapping("/")
+    public List<Theater> getAllTheater() {
+        return theaterService.findAll();
+    }
+
+}
+
